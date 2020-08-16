fe494da8b8835d0e55340ca12f377779bf04cabf
Martin Rubinsztein <martin.rubinsztein@rokt.com>
2020-08-16 11:42:13 +1000

    create extractor with and without memo

--- tests/extractors.test.js
+++ tests/extractors.test.js
@@ -1,4 +1,4 @@
-import { createExtractor } from '../src/extractors';
+import { createExtractor, createMemoExtractor } from '../src/extractors';
 
 describe('Extractors', () => {
   describe('createExtractor', () => {
@@ -10,7 +10,7 @@ describe('Extractors', () => {
       expect(extractorMatrix).toEqual(expect.any(Function));
     });
 
-    it('should return the same function for the same arguments', () => {
+    it('should return the different function for the same arguments', () => {
       const extractorMatrix = createExtractor(() => {
         return true;
       });
@@ -18,7 +18,7 @@ describe('Extractors', () => {
       const extractor1 = extractorMatrix('parameter 1');
       const extractor2 = extractorMatrix('parameter 1');
 
-      expect(extractor1).toBe(extractor2);
+      expect(extractor1).not.toBe(extractor2);
     });
 
     it('should return the different functions for different arguments', () => {
@@ -31,57 +31,141 @@ describe('Extractors', () => {
 
       expect(extractor1).not.toBe(extractor2);
     });
+
+    describe('extractor use', () => {
+      it('should return the value from state', () => {
+        const st = {
+          value: 12,
+        };
+  
+        const extractorMatrix = createExtractor((state) => {
+          return state.value;
+        });
+  
+        const extractor = extractorMatrix();
+  
+        expect(extractor(st)).toEqual(12);
+      });
+  
+      it('should return the right value depending of the argument', () => {
+        const st = {
+          key1: 12,
+          key2: 24,
+        };
+  
+        const extractorMatrix = createExtractor((state, key) => {
+          return state[key];
+        });
+  
+        const extractor1 = extractorMatrix('key1');
+        const extractor2 = extractorMatrix('key2');
+  
+        expect(extractor1(st)).toEqual(12);
+        expect(extractor2(st)).toEqual(24);
+      });
+  
+      it('should return the right value depending of the state', () => {
+        const st1 = {
+          key1: 12,
+        };
+  
+        const st2 = {
+          key1: 24,
+        };
+  
+        const extractorMatrix = createExtractor((state, key) => {
+          return state[key];
+        });
+  
+        const extractor = extractorMatrix('key1');
+  
+        expect(extractor(st1)).toEqual(12);
+        expect(extractor(st2)).toEqual(24);
+      });
+    });
   });
 
-  describe('extractor use', () => {
-    it('should return the value from state', () => {
-      const st = {
-        value: 12,
-      };
-
-      const extractorMatrix = createExtractor((state) => {
-        return state.value;
+  describe('createMemoExtractor', () => {
+    it('should return a new function', () => {
+      const extractorMatrix = createMemoExtractor(() => {
+        return true;
       });
 
-      const extractor = extractorMatrix();
-
-      expect(extractor(st)).toEqual(12);
+      expect(extractorMatrix).toEqual(expect.any(Function));
     });
 
-    it('should return the right value depending of the argument', () => {
-      const st = {
-        key1: 12,
-        key2: 24,
-      };
-
-      const extractorMatrix = createExtractor((state, key) => {
-        return state[key];
+    it('should return the same function for the same arguments', () => {
+      const extractorMatrix = createMemoExtractor(() => {
+        return true;
       });
 
-      const extractor1 = extractorMatrix('key1');
-      const extractor2 = extractorMatrix('key2');
+      const extractor1 = extractorMatrix('parameter 1');
+      const extractor2 = extractorMatrix('parameter 1');
 
-      expect(extractor1(st)).toEqual(12);
-      expect(extractor2(st)).toEqual(24);
+      expect(extractor1).toBe(extractor2);
     });
 
-    it('should return the right value depending of the state', () => {
-      const st1 = {
-        key1: 12,
-      };
-
-      const st2 = {
-        key1: 24,
-      };
-
-      const extractorMatrix = createExtractor((state, key) => {
-        return state[key];
+    it('should return the different functions for different arguments', () => {
+      const extractorMatrix = createMemoExtractor(() => {
+        return true;
       });
 
-      const extractor = extractorMatrix('key1');
+      const extractor1 = extractorMatrix('parameter 1');
+      const extractor2 = extractorMatrix('parameter 2');
 
-      expect(extractor(st1)).toEqual(12);
-      expect(extractor(st2)).toEqual(24);
+      expect(extractor1).not.toBe(extractor2);
+    });
+
+    describe('extractor use', () => {
+      it('should return the value from state', () => {
+        const st = {
+          value: 12,
+        };
+  
+        const extractorMatrix = createMemoExtractor((state) => {
+          return state.value;
+        });
+  
+        const extractor = extractorMatrix();
+  
+        expect(extractor(st)).toEqual(12);
+      });
+  
+      it('should return the right value depending of the argument', () => {
+        const st = {
+          key1: 12,
+          key2: 24,
+        };
+  
+        const extractorMatrix = createMemoExtractor((state, key) => {
+          return state[key];
+        });
+  
+        const extractor1 = extractorMatrix('key1');
+        const extractor2 = extractorMatrix('key2');
+  
+        expect(extractor1(st)).toEqual(12);
+        expect(extractor2(st)).toEqual(24);
+      });
+  
+      it('should return the right value depending of the state', () => {
+        const st1 = {
+          key1: 12,
+        };
+  
+        const st2 = {
+          key1: 24,
+        };
+  
+        const extractorMatrix = createMemoExtractor((state, key) => {
+          return state[key];
+        });
+  
+        const extractor = extractorMatrix('key1');
+  
+        expect(extractor(st1)).toEqual(12);
+        expect(extractor(st2)).toEqual(24);
+      });
     });
   });
 });
