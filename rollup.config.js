250db957794d4e10bc0611dc7531fc6e52a6ffe7
Martin Rubinsztein <martin.rubinsztein@gmail.com>
2021-07-07 17:38:00 +1000

    Suport both umd and esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,22 @@
 // rollup.config.js
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'index.js',
-  output: {
-    name: 'flux-condenser',
-    file: 'build/index.js',
-    format: 'umd',
-    plugins: [
-      terser(),
-    ]
-  },
-  plugins: [
-    resolve(),
-    babel({ babelHelpers: 'bundled' }),
+  input: 'main.js',
+  output: [
+    {
+      name: 'flux-condenser',
+      file: 'build/index.js',
+      format: 'umd',
+      plugins: [terser()],
+    },
+    {
+      file: 'build/index.mjs',
+      format: 'es',
+      plugins: [terser()],
+    },
   ],
+  plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
 };
